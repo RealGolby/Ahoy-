01e579068b7f15885ac2f8c2e91b6550a771a713
RealGolby <73080347+RealGolby@users.noreply.github.com>
2022-10-13 07:58:42 +0200

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public enum PlayerState
 {
-    Idle, Moving, Running, Dead
+    Idle, Moving, Running, Dashing, Dead
+}
+
+public enum PlayerFace
+{
+    Left, Right, Up, Down
 }
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] float jumpForce;
+    [SerializeField] float dashForce;
 
     [SerializeField] PlayerState playerState;
+    [SerializeField] PlayerFace playerFace;
 
     Rigidbody2D rb;
+    SpriteRenderer sr;
+
     [SerializeField] bool isGrounded;
 
     [SerializeField] LayerMask groundLayer;
 
     public int JumpsRemaining = 2;
 
+    float moveX;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
     private void Update()
     {
@@ -33,9 +46,10 @@ public class PlayerMovement : MonoBehaviour
 
     void jump()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
         if (hit.collider == null) isGrounded = false;
-        else {
+        else
+        {
             isGrounded = true;
         }
 
@@ -46,44 +60,77 @@ public class PlayerMovement : MonoBehaviour
                 rb.AddForce(new Vector2(0, jumpForce));
                 JumpsRemaining--;
             }
-            else if(JumpsRemaining > 0 && !isGrounded)
+            else if (JumpsRemaining > 0 && !isGrounded)
             {
                 rb.AddForce(new Vector2(0, jumpForce * 1.5f));
                 JumpsRemaining--;
             }
         }
-     
+
 
     }
     void move()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        moveX = Input.GetAxisRaw("Horizontal");
 
-        if(moveX > 0)
+        if (moveX != 0)
         {
             playerState = PlayerState.Moving;
             if (isGrounded)
             {
-                transform.position += new Vector3(moveX/2 * movementSpeed * Time.deltaTime, 0, 0);
+                transform.position += new Vector3(moveX * movementSpeed * Time.deltaTime, 0, 0);
             }
             else
             {
-                transform.position += new Vector3(moveX * movementSpeed * Time.deltaTime, 0, 0);
+                transform.position += new Vector3(moveX / 1.5f * movementSpeed * Time.deltaTime, 0, 0);
             }
 
+            if (moveX > 0)
+            {
+                sr.flipX = false;
+                playerFace = PlayerFace.Right;
+            }
+            else if (moveX < 0)
+            {
+                sr.flipX = true;
+                playerFace = PlayerFace.Left;
+            }
         }
         else
         {
-            playerState = PlayerState.Idle;   
+            playerState = PlayerState.Idle;
         }
+    }
+
+    void dash()
+    {
+        if (playerState != PlayerState.Moving) return;
+        if (playerFace == PlayerFace.Right && moveX > 0 && Input.GetKeyDown(KeyCode.D))
+        {
+
+            playerState = PlayerState.Dashing;
+            rb.AddForce(new Vector2(dashForce * 1, 0));
+            StartCoroutine("dashCooldown");
+        }
+        else if (playerFace == PlayerFace.Left && moveX < 0 && Input.GetKeyDown(KeyCode.A))
+        {
+            playerState = PlayerState.Dashing;
+            rb.AddForce(new Vector2(dashForce * -1, 0));
+            StartCoroutine("dashCooldown");
+        }
+    }
+
+    IEnumerator dashCooldown()
+    {
+        yield return new WaitForSeconds(2f);
+        playerState = PlayerState.Idle;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 6)
         {
             JumpsRemaining = 2;
-            Debug.Log(":)");
         }
     }
 }
