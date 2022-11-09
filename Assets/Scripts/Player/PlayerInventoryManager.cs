e11e70694c6286038b9994507c2c9f78c9a66487
Vojtěch Kaván <73080347+RealGolby@users.noreply.github.com>
2022-11-09 10:43:59 +0100

    .

--- Assets/Scripts/Player/PlayerInventoryManager.cs
+++ Assets/Scripts/Player/PlayerInventoryManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,23 +6,42 @@ public class PlayerInventoryManager : MonoBehaviour
 {
     [SerializeField] List<Item> items;
 
-    [SerializeField] Item Equppied;
-    [SerializeField] Item lastEquppied;
+    [SerializeField] Item equppiedItem;
+    [SerializeField] Item lastEquppiedItem;
+
+    [SerializeField] GameObject equppiedItemInstance;
+    [SerializeField] GameManager lastEquppiedItemInstance;
 
     [SerializeField] List<Image> HotbarItemsImages;
 
+    [SerializeField] GameObject itemHolder;
+
+    private void Start()
+    {
+        items[0] = equppiedItem;
+    }
+
     private void Update()
     {
-        switchItem();
+        getInput();
     }
 
-    void switchItem()
+    void getInput()
     {
-        lastEquppied = Equppied;
-
+        if (Input.GetKeyDown(KeyCode.Alpha1) && items[0] != null) switchItem(items[0]);
+        if (Input.GetKeyDown(KeyCode.Alpha2) && items[1] != null) switchItem(items[1]);
+        if (Input.GetKeyDown(KeyCode.Alpha3) && items[2] != null) switchItem(items[2]);
+        if (Input.GetKeyDown(KeyCode.Alpha4) && items[3] != null) switchItem(items[3]);
+        if (Input.GetKeyDown(KeyCode.Alpha5) && items[4] != null) switchItem(items[4]);
     }
 
-    void pickItem()
+    void switchItem(Item item)
+    {
+        lastEquppiedItem = equppiedItem;
+        equppiedItem = item;
+    }
+
+    void pickItem(GameObject item)
     {
 
     }
@@ -31,5 +49,15 @@ public class PlayerInventoryManager : MonoBehaviour
     void dropItem()
     {
 
+    }
+
+    GameObject droppedItem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<ItemInstance>(out ItemInstance instance))
+        {
+            droppedItem = collision.gameObject;
+        }
     }
 }
